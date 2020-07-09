33b059cd41ff60021b4707a1c3ada5b19955eb67
Austin_Keener <austingraphicandweb@gmail.com>
2020-07-08 20:00:18 -0400

    creation of function begin() and supporting functions

--- app.js
+++ app.js
@@ -1,33 +1,111 @@
-var mysql = require("mysql");
-var inquirer = require("inquirer");
+// this is where i am going to be writing my inquirer functions that will prompt the user for questions in the cli and the information will then be stored within the database.
+const inquirer = require("inquirer");
+const mysql = require("mysql");
 
 // create the connection information for the sql database
 var connection = mysql.createConnection({
-  host: "localhost",
-
-  // Your port; if not 3306
-  port: 3306,
-
-  // Your username
-  user: "root",
-
-  // Your password
-  password: "mysqlrules!",
-  database: "employee_tracker"
-});
-
-// connect to the mysql server and sql database
-connection.connect(function(err) {
-  if (err) throw err;
-  console.log("connected as id " + connection.threadId + "\n");
-});
-
-function readData(){
-  connection.query("SELECT * FROM characters", function(err, data){
-    if (err) throw err;
-    console.log(data);
-    for(i=0;i<data.length;i++){
-      console.log(`${data[i]} | ${data[i].name}`);
-    }
+    host: "localhost",
+  
+    // Your port; if not 3306
+    port: 3306,
+  
+    // Your username
+    user: "root",
+  
+    // Your password
+    password: "mysqlrules!",
+    database: "employee_tracker"
   });
-}
+  
+  // connect to the mysql server and sql database
+connection.connect(function(err) {
+    if (err) throw err;
+    // run the start function after the connection is made to prompt the user
+    //insert the function name asking questions here.
+  });
+
+  function begin(){
+    inquirer.prompt({
+        name: "start",
+        type: "list",
+        message: "What do you want to do?",
+        choices: ["Add information", "View database content"]
+        }).then (response => {
+    for(i=0;i<choices.length;i++){
+        if(response.choices === [0]){
+          addInfo();
+        } else if (response.choices === [1]) {
+          viewDatabase();
+        }
+    };
+
+  });
+  };
+  begin();
+  
+  function addInfo(){
+    inquirer.prompt
+    (
+        {
+            type:"list",
+            message:"What would you like to add to the database",
+            choices:["department","role","employee"],
+            name:"add"
+        }
+    ).then(response => {
+        if (response.choices === [0]){
+            department();
+        } else if (response.choices === [1]){
+            role();
+        } else if (response.choices === [2]){
+            employee();
+        }
+    })
+  };
+
+  function viewDatabase(){
+    inquirer.prompt 
+    (
+        {
+        
+            type:"list",
+            message:"What would you like to view in the database",
+            choices:["department","role","employee"],
+            name:"add"
+
+        }
+    ).then(response => {
+
+    })
+  };
+
+  function department(){
+    inquirer.prompt
+    (
+        {
+            type:"input",
+            message:"department",
+            name:"department"
+        }
+    )
+  };
+
+  function role(){
+    (
+        {
+            type:"input",
+            message:"what is the role",
+            name:"role"
+        }
+    )
+  };
+
+  function employee(){
+    (
+        {
+            type:"input",
+            message:"name",
+            name:"name"
+        }
+    )
+  };
